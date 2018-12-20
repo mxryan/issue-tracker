494e0b7582e4a2c256b496ec6c5d7bf6297dc993
M Ryan <mxryan@live.com>
2018-12-20 14:47:50 -0500

    added initial db directory

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
+// const mongoose = require("mongoose");
+// const db = require("./models");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(helmet());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
