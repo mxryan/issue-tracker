2f999e9ff46a0565b6d9804fac8c65a8b60d5c30
M Ryan <mxryan@live.com>
2019-01-01 07:53:55 -0500

    removed unnecessary loc

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import IssuePage from "./pages/IssuePage";
 import ProjectPage from "./pages/ProjectPage";
-import NewProjectForm from "./components/NewProjectForm";
-import NewIssueForm from "./components/NewIssueForm";
-import UpdateIssueForm from './components/UpdateIssueForm';
 
 class App extends Component {
   constructor(props) {
@@ -13,12 +10,6 @@ class App extends Component {
       selectedProject: null
     }
   }
-
-  // setPage = (pageName) => {
-  //   this.setState({
-  //     page: pageName
-  //   });
-  // }
 
   setProject = (projectName) => {
     this.setState({
@@ -40,15 +31,7 @@ class App extends Component {
       width: "90%",
       margin: "auto"
     }
-    const flexContainer = {
-      display: "flex",
-      justifyContent: "space-around"
-    }
-    const flexItem = {
-      width: "380px",
-      border: "1px solid black",
-      padding: "20px"
-    }
+   
 
     const currentPage = this.state.page === "projects"
       ? <ProjectPage setProject={this.setProject}/>
@@ -56,29 +39,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <div style={container}>
-            <h1>Issue Tracker</h1>
-          </div>
-        </header>
-
-        <div style={container}>
-          <h1>OLD</h1>
-          <div style={flexContainer}>
-            <div style={flexItem}>
-              <NewProjectForm />
-            </div>
-            <div style={flexItem}>
-              <NewIssueForm />
-            </div>
-          </div>
-          <div style={flexContainer}>
-            <div style={flexItem}><UpdateIssueForm /></div>
-            <div style={flexItem}>new issue</div>
-            
-          </div>
-        </div>
-        <hr/>
+        
         <div style={container}>
           <h1>New</h1>
           {currentPage}
